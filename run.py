00335d6ebc6c440a2a72a99ae8ae987aea7e6405
E Rippey <rippey.e@gmail.com>
2018-12-09 19:46:01 -0800

    Support for STL output

--- run.py
+++ run.py
@@ -5,6 +5,8 @@ from commands import getstatusoutput
 from tempfile import NamedTemporaryFile
 
 def do_or_print(s):
+	#str->void
+
 	status,output=getstatusoutput(s)
 	if status:
 		print 'Command failed:',s
@@ -12,16 +14,39 @@ def do_or_print(s):
 		exit(1)
 
 
-def main():
-	p=ArgumentParser()
-	p.add_argument('-o','--output_file',default='output.svg')
-	args=p.parse_args()
+def stl(input_file,output_file):
+	#str->str->void
+	tmp=NamedTemporaryFile(suffix='.scad')
+	print>>tmp,open(input_file,'r').read().replace('projection()','')
+	tmp.flush()
+	do_or_print('openscad '+tmp.name+' -o '+output_file)
+
+
+def svg(input_file,output_file):
+	#str->str->void
 	tmp=NamedTemporaryFile(suffix=".dxf")
 
-	do_or_print('openscad parametric_involute_gear_v5.0.scad -o '+tmp.name)
+	do_or_print('openscad '+input_file+' -o '+tmp.name)
 
 	#25.4 is to convert from inches to mm.
-	do_or_print('/usr/share/inkscape/extensions/dxf_input.py --scale=25.4 '+tmp.name+' > '+args.output_file)
+	do_or_print('/usr/share/inkscape/extensions/dxf_input.py --scale=25.4 '+tmp.name+' > '+output_file)
+
+
+def main():
+	p=ArgumentParser()
+	p.add_argument('-i','--input_file',default='parametric_involute_gear_v5.0.scad')
+	p.add_argument('-o','--output_file',default='output.svg')
+	args=p.parse_args()
+
+	if args.output_file.endswith('.stl'):
+		run=stl
+	elif args.output_file.endswith('.svg'):
+		run=svg
+	else:
+		print>>stderr,'Error: Unrecognized format for output'
+		return 1
+
+	return run(args.input_file,args.output_file)
 
 
 if __name__=='__main__':
